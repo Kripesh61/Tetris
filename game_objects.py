f087f0326b793271396e9a8cdab0f33db3b90a04
Kripesh <dhungelkripesh@gmail.com>
2026-04-01 04:47:08 +0000

    Apr 1, 2026, 12:47 AM

--- game_objects.py
+++ game_objects.py
@@ -9,3 +9,8 @@ class Piece:
         self.y = y
         self.shape = shape
         self.is_bomb = is_bomb
+        if is_bomb: self.shape = [[1]]
+
+        def get_high_score():
+            if not os.path.exists(HS_FILE): return 0
+            
