577049842436f463684e6983849ca5f4026bc1d0
Kripesh <dhungelkripesh@gmail.com>
2026-04-01 04:32:54 +0000

    Apr 1, 2026, 12:32 AM

--- constants.py
+++ constants.py
@@ -11,7 +11,7 @@ HS_FILE = "highscore.txt"
 # Colors
 
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+BLACK = (0, 0, 0) 
 COLORS = [ (0, 255, 255), (255, 255, 0), (128, 0, 128), (0, 0, 225), (255, 0, 0), (255, 165, 0), (0, 225, 0)   ]
 
 # Custom Shapes: I, O, T, V, X
