7d5a7a3ce8b4863f0513f19e0af491e6829a9230
Kripesh <dhungelkripesh@gmail.com>
2026-04-01 10:00:56 -0400

    pygame

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import os
+from constants import *
+from game_objects import Piece, get_high_score, save_high_score, create_grid, valid_space
+
+# Initialize Pygame
+pygame.init()
+pygame.mixer.init()
+
+
+
