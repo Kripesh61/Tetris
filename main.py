53439cfd63834336906a86472d93115916b2cdba
Kripesh <dhungelkripesh@gmail.com>
2026-04-04 01:59:14 +0000

    Apr 3, 2026, 9:59 PM

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ pygame.mixer.init()
 def load_sound(file):
     return pygame.mixer.Sound(file) if os.path.exists(file) else None
 
+# Load Assets
+sfx_place = load_sound("place.wav")
+sfx_explode = load_sound("explode.wav")
+sfx_gameover = load_sound("gameover.wav")
+
+def play_bgm():
+    if os.path.exists("bgm.mp3"):
+        pygame.mixer.music.load("bgm.mp3")
+        pygame.mixer.music.set_volume(0.4)
+        pygame.mixer.music.play(-1)
+
+
+
+
 
 
 
