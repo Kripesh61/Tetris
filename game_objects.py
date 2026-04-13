63d83372b0b6a64e2526c63e3084e1507e8d2cb2
Kripesh <dhungelkripesh@gmail.com>
2026-04-13 14:54:09 +0000

    Apr 13, 2026, 10:54 AM

--- game_objects.py
+++ game_objects.py
@@ -11,23 +11,23 @@ class Piece:
         self.is_bomb = is_bomb
         if is_bomb: self.shape = [[1]]
 
-    def get_high_score():
+def get_high_score():
             if not os.path.exists(HS_FILE): return 0
             with open(HS_FILE, "r") as f:
                 try: return int(f.read())
                 except: return 0
 
-    def save_high_score(self,new_score):
+def save_high_score(self,new_score):
              if new_score > self.get_high_score():
                 with open(HS_FILE, "w") as f: f.write(str(new_score))
 
-    def create_grid(locked_pos={}):
+def create_grid(locked_pos={}):
          grid = [[BLACK for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
          for (x, y), color in locked_pos.items():
               if y >= 0: grid[y][x] = color
               return grid
          
-    def valid_space(piece, grid):
+def valid_space(piece, grid):
          accepted_pos = [(j, i) for i in range(GRID_HEIGHT) for j in range(GRID_WIDTH) if grid[i][j] == BLACK]
          for y, row in enumerate(piece.shape):
              for x, val in enumerate(row):
