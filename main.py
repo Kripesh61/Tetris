275748ec4696d0ab5be7ca879f5ed9388510e0f4
Kripesh <dhungelkripesh@gmail.com>
2026-04-13 14:51:35 +0000

    Apr 13, 2026, 10:51 AM

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import os
 from constants import *
-from game_objects import *
+from game_objects import  Piece, get_high_score, save_high_score, create_grid, valid_space
 
 # Initialize Pygame
 pygame.init()
