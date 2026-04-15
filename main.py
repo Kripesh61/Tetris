c0380437c0cb0bed1e9742a45134f81ab9b39db4
Kripesh <dhungelkripesh@gmail.com>
2026-04-14 20:48:07 -0400

    sound

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.init()
 pygame.mixer.init()
 
 def load_sound(file):
-    return pygame.mixer.Sound(file) if os.path.exists(file) else None
+    
 
 # Load Assets
 sfx_place = load_sound("place.wav")
